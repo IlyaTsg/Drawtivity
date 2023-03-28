54db24e92bfa3e32b1b16622ad2595b76dcf6a84
No1ball <noonepoint@gmail.com>
2023-03-29 00:42:55 +0300

    init frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from "react";
+import SignUp from "./components/Authorization/SignUpPage/components/SignUp";
+
+
+function App() {
+  return (
+    <div className="App">
+        <SignUp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Authorization/SignUpPage/components/SignUp.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {useForm} from "react-hook-form";
+import classes from "../../SignUpPage/components/SignUp.module.scss";
+import CustomForm from "../../UI/Form/CustomForm";
+import InputEmail from "../../UI/InputEmail/InputEmail";
+import PasswordInput from "../../UI/PasswordInput/PasswordInput";
+import InputName from "../UI/InputName/InputName";
+const SignUp = () => {
+    const {
+        register,
+        formState:{
+            errors,
+            isValid
+        },
+        handleSubmit,
+        reset,
+    } = useForm({mode:'onChange'})
+    const submitHandler = (data) =>{
+        alert(JSON.stringify(data))
+        reset()
+    }
+    return (
+        <div className={classes.wrapper}>
+            <CustomForm btnText={'Sign In'}
+                        submitHandler={submitHandler}
+                        handlerSubmit={handleSubmit}
+                        isValid={isValid}
+                        formCl={classes.form}
+            >
+                <InputName label={'First name'} errors={errors} register={register} name={'firstName'}/>
+                <InputName label={'Second name'} errors={errors} register={register} name={'secondName'}/>
+                <InputEmail errors={errors} register={register}/>
+                <PasswordInput errors={errors} register={register}/>
+            </CustomForm>
+        </div>
+    );
+};
+
+export default SignUp;
