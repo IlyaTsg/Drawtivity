08e7f316f2b3d3267833211e32328be3c3ae5dba
No1ball <noonepoint@gmail.com>
2023-05-27 22:33:09 +0300

    add components and pages

--- frontend/src/components/TaskPage/components/TaskPage.jsx
+++ frontend/src/components/TaskPage/components/TaskPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import CenterMainBlock from "../../../entities/ui/centerMainBlock/CenterMainBlock";
+import TaskBlock from "../../../widgets/ui/TaskBlock/TaskBlock";
 
 const TaskPage = () => {
     return (
-        <div>
-
-        </div>
+        <CenterMainBlock>
+            <TaskBlock/>
+        </CenterMainBlock>
     );
 };
 
--- frontend/src/widgets/ui/TaskItem/TaskItem.jsx
+++ frontend/src/widgets/ui/TaskItem/TaskItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './TaskItem.module.scss'
-const TaskItem = ({ind, title, category, type}) => {
+import {Link} from "react-router-dom";
+const TaskItem = ({ind, title, category, type, moreClickHandler}) => {
     return (
         <div className={classes.itemRow}>
             <span className={classes.indClasses}>{ind}</span>
@@ -9,7 +10,7 @@ const TaskItem = ({ind, title, category, type}) => {
             </div>
             <div className={classes.itemRow__item}>{category}</div>
             <div className={classes.itemRow__item}>{type}</div>
-            <div className={classes.itemRow__item}>Подробнее</div>
+            <Link className={classes.itemRow__item} to={`/task/${ind}`}>Подробнее</Link>
 
         </div>
     );
