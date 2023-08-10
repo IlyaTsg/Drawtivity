16a827c17a73c7c910a941468629db536f728503
IlyaTsg <saw44947@gmail.com>
2023-08-10 20:50:22 +0300

    Add task solution for tasks

--- api/src/main/java/com/ETU/config/AppConfig.java
+++ api/src/main/java/com/ETU/config/AppConfig.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(exclude= HibernateJpaAutoConfiguration.class)
 @ComponentScan("com.ETU")
+@Import(HibernateConfiguration.class)
 public class AppConfig extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(AppConfig.class, args);
