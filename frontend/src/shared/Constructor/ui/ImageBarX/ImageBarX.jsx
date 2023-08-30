ab5ae889110872949dd03993aa7c840bd73e582e
No1ball <noonepoint@gmail.com>
2023-08-30 13:54:28 +0300

    Редизайн конструктора

---
+++ frontend/src/shared/Constructor/ui/ImageBarX/ImageBarX.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from "./ImageBarX.module.scss";
+const ImageBarX = () => {
+    return (
+        <div className={classes.wrap}>
+          <div>
+              0
+          </div>
+          <div className={classes.last}>
+              770
+          </div>
+        </div>
+    );
+};
+
+export default ImageBarX;
